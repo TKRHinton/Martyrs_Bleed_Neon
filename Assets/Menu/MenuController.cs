c0513e79a5f98ff71c29b7424e034120814e7190
ThomasH <thomashinton14@gmail.com>
2022-10-07 00:36:17 +0100

    Exit Button Functionality

---
+++ Assets/Menu/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+	[Header("here")]
+	public string _newGameLevel;
+	private string levelToLoad;
+	[SerializeField] private GameObject noSaveGameDialog = null;
+	
+	
+	
+	
+	
+	public void ExitButton()
+	{
+		Application.Quit();
+	}
+	
+	
+
+
+}
