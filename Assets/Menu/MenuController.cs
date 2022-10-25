7b98314b5209342cb94c7df32b83edf9cc4ca78c
ThomasH <thomashinton14@gmail.com>
2022-10-25 18:46:21 +0100

    Fixed Settings Buttons

--- Assets/Menu/MenuController.cs
+++ Assets/Menu/MenuController.cs
@@ -27,14 +27,17 @@ public class MenuController : MonoBehaviour
 	
 	private void Start()
 	{
-		resolutions = Screen.resolutions;
-		resolutionDropdown.ClearOptions();
+		PlayerPrefs.DeleteAll();
+		Debug.Log ("Start!!");
+		
+		resolutions = Screen.resolutions; // Collects Users Screen Resolutions
+		resolutionDropdown.ClearOptions(); // Clears Drop down
 		
 		List<string> options = new List<string>();
 		
 		int currentResolutionsIndex = 0;
 		
-		for (int i = 0; i < resolutions.Length; i++) {
+		for (int i = 0; i < resolutions.Length; i++) { //collects all resolutions in an array 
 			
 			string option = resolutions[i].width + " x " + resolutions[i].height;
 			options.Add(option);
@@ -46,6 +49,7 @@ public class MenuController : MonoBehaviour
 				
 		}
 		
+		//Adds array to dropdown
 		resolutionDropdown.AddOptions(options);
 		resolutionDropdown.value = currentResolutionsIndex;
 		resolutionDropdown.RefreshShownValue();
@@ -53,6 +57,8 @@ public class MenuController : MonoBehaviour
 	
 	public void SetResolution(int resolutionIndex)
 	{
+		//Set Resolution of Game 
+		Debug.Log (resolutions[resolutionIndex]);
 		Resolution resolution = resolutions[resolutionIndex];
 		Screen.SetResolution(resolution.width,resolution.width, Screen.fullScreen);
 	}
@@ -87,14 +93,26 @@ public class MenuController : MonoBehaviour
 	}
 	
 	
-	public void SetFullScreen(string isFullScreen)
+	public void SetFullScreen(int isFullScreen)
 	{
-		Debug.Log("Output" + isFullScreen + "ffff");
 		
-		//	_isFullscreen = isFullScreen;
 		
-		//	PlayerPrefs.SetInt("masterFullscreen", (_isFullscreen ? 1 : 0));
-		//	Screen.fullScreen = _isFullscreen;
+		
+		bool fullScreen = true;
+		
+		if (isFullScreen == 0)
+		{
+			fullScreen = true;
+		}
+		else {fullScreen = false;}
+		
+		
+		_isFullscreen = fullScreen;
+		
+		Debug.Log(fullScreen);
+		
+		PlayerPrefs.SetInt("masterFullscreen", (_isFullscreen ? 1 : 0));
+		Screen.fullScreen = _isFullscreen;
 		
 	}
 	
@@ -104,6 +122,8 @@ public class MenuController : MonoBehaviour
 		
 		PlayerPrefs.SetInt("masterQuality", _qualityLevel);
 		QualitySettings.SetQualityLevel(_qualityLevel);
+		Debug.Log(QualityIndex);
+		Debug.Log(QualitySettings.currentLevel); //log quality level
 	}
 
 
